ce3b41228b36eeeba3ed51805cf048fcb5cd16c0
prashanth <prashanthh036@gmail.com>
2024-07-19 22:25:02 +0530

    Third commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your React app's URL
+  origin: 'https://prashanth036.github.io/techAssistFrontEnd/', // Replace with your React app's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allow credentials (cookies, authorization headers)
 };
